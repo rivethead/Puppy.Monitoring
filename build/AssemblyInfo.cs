0047fc8e3c1b38278c5f8dd1cba49b54f98b35fa
Pieter <pieter.schutte@structureit.net>
2013-02-21 17:42:56 +0200

    build script to package

---
+++ build/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyDescription("Puppy.Monitoring")]
+[assembly: AssemblyProduct("Puppy.Monitoring")]
+[assembly: AssemblyVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+
+[assembly: ComVisibleAttribute(false)]
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyInformationalVersion("0.0.0.ce38d6")]
+
