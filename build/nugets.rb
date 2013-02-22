757e57f0c9ea9e93a43a24540e9a931e6b179071
rivethead <schutte.pieter@gmail.com>
2013-02-22 08:46:52 +0200

    nuget package for sql server publisher

--- build/nugets.rb
+++ build/nugets.rb
@@ -11,5 +11,20 @@
 		:dependencies => [
 			['Common.Logging', '[2.1.1]']
 		]
-	}
+	},
+	{
+		:package_id => 'Puppy.Monitoring.SqlServerPublisher',
+		:description => 'Puppy.Monitoring.SqlServerPublisher',
+		:authors => 'rivethead_',
+		:base_folder => 'Puppy.Monitoring/Publishers/Puppy.Monitoring.SqlServerPublisher/',
+		:files => [
+			['Puppy.Monitoring.SqlServerPublisher.dll', 'lib\net45'],
+			['Puppy.Monitoring.SqlServerPublisher.pdb', 'lib\net45'],
+			['rakefile', ''],
+		],
+		:dependencies => [
+			['Common.Logging', '[2.1.1]'],
+			['Puppy.Monitoring', ''],
+		]
+	}	
 ]
