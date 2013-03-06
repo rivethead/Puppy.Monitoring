209ce73e503cc075759c0c935e2647b08bfcdbd4
rivethead <schutte.pieter@gmail.com>
2013-03-06 12:48:34 +0200

    build artifacts

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Security;
 [assembly: AssemblyDescription("Puppy.Monitoring")]
 [assembly: AssemblyProduct("Puppy.Monitoring")]
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.40")]
+[assembly: AssemblyFileVersion("0.1.41")]
 
-[assembly: AssemblyInformationalVersion("0.1.40.543d24")]
+[assembly: AssemblyInformationalVersion("0.1.41.3b8e57")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
 
