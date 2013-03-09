2b5b232e96f68cf2da708019a101f78adbbb7f69
rivethead <schutte.pieter@gmail.com>
2013-03-09 12:43:53 +0200

    build artifact

--- build/AssemblyInfo.cs
+++ build/AssemblyInfo.cs
@@ -7,7 +7,6 @@ using System.Security;
 [assembly: AssemblyVersion("0.1.0")]
 [assembly: AssemblyFileVersion("0.1.14")]
 
-[assembly: AssemblyInformationalVersion("0.1.14.de76a5")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: CLSCompliantAttribute(false)]
-
+[assembly: AssemblyInformationalVersion("0.1.14.a30486")]
